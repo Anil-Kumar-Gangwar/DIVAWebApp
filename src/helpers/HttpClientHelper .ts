73aa86e1cf1ea82728fc47d9be6bf843d3f63830
Anil Kumar <anil_kumarbly@hotmail.com>
2022-07-11 09:30:33 +0530

    commited

--- src/helpers/HttpClientHelper .ts
+++ src/helpers/HttpClientHelper .ts
@@ -1,8 +1,12 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export class HttpClientHelper {
-  static baseURL: string = 'http://localhost:50323/apigateway';
-  static auth_GateWay = 'http://localhost:50323/auth/microservice';
+  static baseURL: string =
+    'http://apigateway.digitalvirtualastt.org/apigateway';
+
+  static auth_GateWay =
+    'http://apigateway.digitalvirtualastt.org/auth/microservice';
+
   static httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
